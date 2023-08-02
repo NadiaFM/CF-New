a3363908d3766e4ff41f7a51d86f40bbc3221895
SAIFULLAH SHAIKH <ssaifullah272@gmail.com>
2023-08-02 18:12:58 +0500

    Update api_endpoint.py

--- ol_messenger_integration/models/api_endpoint.py
+++ ol_messenger_integration/models/api_endpoint.py
@@ -23,10 +23,11 @@ class MessengerIntegrationController(http.Controller):
 
         # # return http.Response(json.dumps(response_data), status=200, content_type='application/json')
         # Parse the query params
-        mode = kw.get('hub.mode')
+        # mode = kw.get('hub.mode')
+        mode = request.args.get('hub.mode')
         token = kw.get('hub.verify_token')
         challenge = kw.get('hub.challenge')
-        raise UserError(token)
+        raise UserError(mode)
 
         # Replace 'config.verifyToken' with your actual verify token value
         verify_token = 'EAA0GF4cZCxPkBO5S1NneUhzsR0p64t9tWO0kpdp442Wam0DE72twPYp7ZAZAR0ENHqBkEHIKV0RskcIHorvNQRyJkmcjX31EhsMvPvONN2fUuPKQZAJWhh8t3E4xsbvrBSQeg5MVo91A1CH8qeZBaA6kMmnp9lf8QwOPRZAjlJecaJUSZBEbK4WyZACvUxPh8jq8'
