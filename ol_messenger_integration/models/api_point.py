7d686f67738a411a2f09b4a566cb15b445fc6020
SAIFULLAH SHAIKH <ssaifullah272@gmail.com>
2023-08-03 17:09:56 +0500

    Update api_point.py

--- ol_messenger_integration/models/api_point.py
+++ ol_messenger_integration/models/api_point.py
@@ -11,7 +11,7 @@ class WebhookController(http.Controller):
             # Handle the webhook verification
             verify_token = post.get('hub.verify_token')
             hub_challenge = post.get('hub.challenge')
-            if verify_token == 'hello':
+            if verify_token == 'EAA0GF4cZCxPkBO2R9CiTaHB8xZAzZAjIEtggXmRwjoUljZBGbYeoPyZCtYrZAyVaXmEZBa2mrExYoTfjnlIsxcGLZA5kNl5czGxzzy5c0nK2vTzRg4cGZCl1v2CZAZBLVhZARNZB30FKnYFfL3bzZAyfjUGBM6ubEplyOO5CvfKB87FXdqzZBxF8J3hAGBjYYe7wRHCz6jv':
                 return Response(hub_challenge, content_type='text/plain', status=200)
             else:
                 return Response("Invalid verify token", content_type='text/plain', status=403)
