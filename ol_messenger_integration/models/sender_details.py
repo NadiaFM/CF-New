e3448d67fd793f3754be8082c38c7708a8bfa311
SAIFULLAH SHAIKH <ssaifullah272@gmail.com>
2023-08-10 16:04:42 +0500

    Update sender_details.py

--- ol_messenger_integration/models/sender_details.py
+++ ol_messenger_integration/models/sender_details.py
@@ -1,6 +1,6 @@
-from odoo import models, fields
+# from odoo import models, fields
 
-class WebhookData(models.Model):
-    _inherit = 'res.partner'
+# class WebhookData(models.Model):
+#     _inherit = 'res.partner'
 
-    sender_id = fields.Char(string='Sender ID')
+#     sender_id = fields.Char(string='Sender ID')
