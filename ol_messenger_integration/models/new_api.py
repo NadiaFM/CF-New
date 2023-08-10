652a150452024cde3678c729b21ce21546925c13
SAIFULLAH SHAIKH <ssaifullah272@gmail.com>
2023-08-10 11:35:57 +0500

    Update new_api.py

--- ol_messenger_integration/models/new_api.py
+++ ol_messenger_integration/models/new_api.py
@@ -34,16 +34,27 @@ class WebhookController(http.Controller):
             for entry in entries:
                 webhookEvent = entry['messaging'][0]
                 # return request.make_response(webhookEvent)
-                # senderPsid = webhookEvent['sender']['id']
+                senderPsid = webhookEvent['sender']['id']
                 # print('sender PSID: {}'.format(senderPsid))
                 if 'message' in webhookEvent:
                     # return Response(webhookEvent, status=200)
                     _logger.info(str(webhookEvent))
-                    # url = "https://graph.facebook.com/{}?fields=id,name,email,picture&access_token={}".format(senderPsid, "EAA0GF4cZCxPkBOwDZBDm92wHtUCgVVuXYTM6NbtkXMzEeQ5wyzeEqhCGsMdeqSeoDjDAH07GZBkWSoCBKPqTZBr6XEbcPb04Lrr4KkbHafeC7rzhqMiZCIKHG0fYSyU6XZA3jgMsQNOqJlu4dm5O9RN3R9qsT009oZAE3yeXu6svFVfOJMmG7bAH5kZCMU2FqGve")
-                    # profile_data = requests.get(url)
-                    # sender_data = profile_data.json()
-                    # _logger.info(str(sender_data))
+                    url = "https://graph.facebook.com/{}?fields=id,name,email,picture&access_token={}".format(senderPsid, "EAA0GF4cZCxPkBOwDZBDm92wHtUCgVVuXYTM6NbtkXMzEeQ5wyzeEqhCGsMdeqSeoDjDAH07GZBkWSoCBKPqTZBr6XEbcPb04Lrr4KkbHafeC7rzhqMiZCIKHG0fYSyU6XZA3jgMsQNOqJlu4dm5O9RN3R9qsT009oZAE3yeXu6svFVfOJMmG7bAH5kZCMU2FqGve")
+                    profile_data = requests.get(url)
+                    sender_data = profile_data.json()
+                    _logger.info(str(sender_data))
+                    request.session['sender_data'] = sender_data
                 # return Response('Ok', status=200)
                 return Response(webhookEvent, status=200)
         else:
             return Response('Error', status=404)
+
+class ProfileController(http.Controller):
+    @http.route('/display_data', type='http', auth='public', methods=['GET'], csrf=False)
+    def display_data(self, **post):
+        # Retrieve sender_data from session
+        sender_data = request.session.get('sender_data')
+        return Response(sender_data, status=200)
+
+        # Now you can use sender_data to display on a webpage or process further
+        # ...
